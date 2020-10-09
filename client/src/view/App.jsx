1f10b8607797f62446b7cfba02944a69fd413f0b
Ritesh Patil <riteshsp2000@gmail.com>
2020-10-10 01:14:54 +0530

    Fix layout

--- client/src/view/App.jsx
+++ client/src/view/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-import { Grid, AppBar, Paper, Toolbar, Typography } from '@material-ui/core';
+import { Grid, AppBar, Paper, Toolbar, Typography, Container } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 import Modal from '../components/Modal';
@@ -57,38 +57,40 @@ function App() {
 
       <div className={classes.toolbar}></div>
 
-      <Grid container spacing={3} className={classes.container}>
-        <Grid item lg={8}>
-          <Paper elevation={2} className={classes.box2}>
-            {rect && (
-              <P5 width={Math.round(rect.width) * 2} height={Math.round(rect.height)} />
-            )}
-          </Paper>
-        </Grid>
+      <Container>
+        <Grid container spacing={3} className={classes.container}>
+          <Grid item sm={12} md={8} lg={8} style={{height: '75%'}}>
+            <Paper elevation={2} className={classes.box2}>
+              {rect && (
+                <P5 width={Math.round(rect.width) * 2} height={Math.round(rect.height)} />
+              )}
+            </Paper>
+          </Grid>
 
-        <Grid item lg={4}>
-          <Paper elevation={2} className={classes.box3} ref={ref}>
-            <h1>Box3</h1>
-          </Paper>
-        </Grid>
+          <Grid item sm={12} md={4} lg={4} style={{height: '75%', marginBottom: 0}}>
+            <Paper elevation={2} className={classes.box3} ref={ref}>
+              <h1>Box3</h1>
+            </Paper>
+          </Grid>
 
-        <Grid item lg={12}>
-          <Paper elevation={2} className={classes.box1}>
-            <h1>{slackCounter}</h1>
-            <button
-              onClick={() => {
-                setModalOpen(true);
-                setTimeout(function () {
-                  setModalOpen(false);
-                }, 5000);
-              }}
-              style={{ marginLeft: 20 }}
-            >
-              open
-            </button>
-          </Paper>
+          <Grid item sm={12} md={12} lg={12} style={{height: '25%', marginBottom: 0}}>
+            <Paper elevation={2} className={classes.box1}>
+              <h1>{slackCounter}</h1>
+              <button
+                onClick={() => {
+                  setModalOpen(true);
+                  setTimeout(function () {
+                    setModalOpen(false);
+                  }, 5000);
+                }}
+                style={{ marginLeft: 20 }}
+              >
+                open
+              </button>
+            </Paper>
+          </Grid>
         </Grid>
-      </Grid>
+      </Container>
 
       {newMember.name !== '' && (
         <Modal
@@ -105,18 +107,31 @@ export default App;
 
 const useStyles = makeStyles((theme) => ({
   container: {
-    backgroundColor: '#F5F5F5',
+    padding: 10,
+    height: window.innerHeight - 80,
+    backgroundColor: ''
   },
-  toolbar: theme.mixins.toolbar,
+  appbar: {
+    height: 80,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    backgroundColor: 'rgba(156, 70, 104, 0.3)'
+  },
+  toolbar: {
+    ...theme.mixins.toolbar, 
+    height: 80
+  },
   box1: {
-    height: '17vh',
+    height: '100%',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#072540',
+
   },
   box2: {
-    height: '68vh',
-    width: '100%',
+    height: '100%',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
@@ -124,9 +139,10 @@ const useStyles = makeStyles((theme) => ({
     overflow: 'hidden'
   },
   box3: {
-    height: '68vh',
+    height: '100%',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#072540',
   },
 }));
